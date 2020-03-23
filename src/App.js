ba78f112b1c481c4a4df08c40f658fa62b6a30b0
talha619 <hafiz.talhakh@gmail.com>
2020-03-23 17:34:50 +0500

    template updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { InputGroup, Button, FormControl } from 'react-bootstrap';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 //Assets
 import logo from './Images/mylogo-1.png';
@@ -14,7 +20,7 @@ function App() {
       <header className="header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <div className="overlay">
-          <h1 style={{ color: 'white', fontSize: '3em', margin: 0, padding: 0, fontWeight: 'normal' }}>Coming Soon</h1>
+          <h1 style={{ color: 'white', fontSize: '3em', margin: 10, padding: 0, fontWeight: 'normal' }}>Coming Soon</h1>
           <p style={{ color: 'white', opacity: '0.5' }}>Stay Tuned</p>
           <InputGroup className="mb-3 custom-form">
             <FormControl
@@ -27,7 +33,40 @@ function App() {
               <Button variant="info">Subscibe</Button>
             </InputGroup.Append>
           </InputGroup>
-
+          {/* <p style={{ color: 'white', opacity: '0.5' }}>Follow Us</p> */}
+          <div style={{ height: 10 }} />
+          <ul className="unordered-list">
+            <li className="list-unstyle">
+              <a href="https://www.facebook.com/pg/technest.com.pk" target="blank" className="link-unstyle">
+                <FacebookIcon style={{ fontSize: '2em' }} />
+              </a>
+            </li>
+            <li className="list-unstyle">
+              <a href="https://www.facebook.com/pg/technest.com.pk" target="blank" className="link-unstyle">
+                <TwitterIcon style={{ fontSize: '2em' }} />
+              </a>
+            </li>
+            <li className="list-unstyle">
+              <a href="https://www.youtube.com/channel/UCISXOO0MW6rIprDaNI0-I0Q" target="blank" className="link-unstyle">
+                <YouTubeIcon style={{ fontSize: '1.8em' }} />
+              </a>
+            </li>
+            <li className="list-unstyle">
+              <a href="https://www.linkedin.com/company/technest-pk" target="blank" className="link-unstyle">
+                <LinkedInIcon style={{ fontSize: '1.8em' }} />
+              </a>
+            </li>
+            <li className="list-unstyle">
+              <a href="https://www.facebook.com/pg/technest.com.pk" target="blank" className="link-unstyle">
+                <YouTubeIcon style={{ fontSize: '1.8em' }} />
+              </a>
+            </li>
+            <li className="list-unstyle">
+              <a href="https://github.com/TechNest-pk" target="blank" className="link-unstyle">
+                <GitHubIcon style={{ fontSize: '1.8em' }} />
+              </a>
+            </li>
+          </ul>
         </div>
       </header>
     </div>
