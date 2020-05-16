b184ce409c1cb6b16cb74ae405664a0ae01a1c6e
talha619 <hafiz.talhakh@gmail.com>
2020-05-17 02:06:16 +0500

    Updates

---
+++ precache-manifest.ce9ef6d8d9366e731aa2f3ac8a10bd5c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6082bcd6a72e5984ed151c51d308a30c",
+    "url": "/coming-soon-page/index.html"
+  },
+  {
+    "revision": "5a635571aeb6ea0d13e8",
+    "url": "/coming-soon-page/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c236a96b977477b78e4b",
+    "url": "/coming-soon-page/static/css/main.851d4deb.chunk.css"
+  },
+  {
+    "revision": "5a635571aeb6ea0d13e8",
+    "url": "/coming-soon-page/static/js/2.01623a25.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/coming-soon-page/static/js/2.01623a25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c236a96b977477b78e4b",
+    "url": "/coming-soon-page/static/js/main.b7b66264.chunk.js"
+  },
+  {
+    "revision": "852df20f85a32d5c3876",
+    "url": "/coming-soon-page/static/js/runtime-main.171755e8.js"
+  },
+  {
+    "revision": "572d5f01b2b0905044007d9845341e77",
+    "url": "/coming-soon-page/static/media/mylogo-1.572d5f01.png"
+  },
+  {
+    "revision": "541cf94883ee86e481d50284f19a181e",
+    "url": "/coming-soon-page/static/media/parallax.541cf948.jpg"
+  }
+]);
